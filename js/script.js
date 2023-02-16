a3712268ec50530041ec865d1a9aa3ecff18051d
Anshu Chauhan <101622044+ac51019@users.noreply.github.com>
2023-02-16 23:55:05 +0530

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,37 @@
+document.getElementById("firstIcon").addEventListener("click", block1)
+document.getElementById("secondIcon").addEventListener("click", block2)
+document.getElementById("thirdIcon").addEventListener("click", block3)
+
+function block1() {
+    var check = document.getElementById("firstUL")
+
+    if (check.style.display == "block") {
+        check.style.display = "none"
+        check.style.listStyleType = "none"
+    } else {
+        check.style.display = "block"
+        check.style.listStyleType = "none"
+    }
+
+}
+function block2() {
+    var check = document.getElementById("secondUL")
+    if (check.style.display == "block") {
+        check.style.display = "none"
+        check.style.listStyleType = "none"
+    } else {
+        check.style.display = "block"
+        check.style.listStyleType = "none"
+    }
+
+}
+function block3() {
+    var check = document.getElementById("thirdUL")
+    if (check.style.display == "block") {
+        check.style.display = "none"
+        check.style.listStyleType = "none"
+    } else {
+        check.style.display = "block"
+        check.style.listStyleType = "none"
+    }
+}
